7f9f734b199960230a7625975fa5e339ca32f48b
laguirre@uamv.edu.ni <laguirrev@uamv.edu.ni>
2026-09-22 20:00:12 -0600

    Uniendo

--- src/main/java/com/example/progevaluacionventana/HelloApplication.java
+++ src/main/java/com/example/progevaluacionventana/HelloApplication.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(
+                HelloApplication.class.getResource("ventana-inicio.fxml")
+        );
+
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Inicio de sesión");
         stage.show();
     }
 }
